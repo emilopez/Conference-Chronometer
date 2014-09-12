0ba3c61d15f58c5742b96bf63f23b8f99e23f89b
ceneha <elopez@fich.unl.edu.ar>
2014-09-12 00:06:39 -0300

    using new-style syntax connecting events

--- ui/qt_chrono_class.py
+++ ui/qt_chrono_class.py
@@ -5,14 +5,24 @@ import nico
 class chrono(QtGui.QMainWindow, nico.Ui_MainWindow):
     def __init__(self,parent = None):
         QtGui.QMainWindow.__init__(self,parent)
-        # se crea la ventana principal vinculandola con la clase previa
+        # crea ventana principal
         self.ventana = nico.Ui_MainWindow()
         self.ventana.setupUi(self)
 
-        # Eventos GUI: pushButton clicked() -> self.putImage
-        self.connect(self.ventana.CloseBtn,QtCore.SIGNAL("clicked()"),QtCore.QCoreApplication.instance().quit)
-        self.connect(self.ventana.LaunchBtn,QtCore.SIGNAL("clicked()"),self.LaunchChrono)
+        # GUI events
+        self.ventana.CloseBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.ventana.LaunchBtn.clicked.connect(self.LaunchChrono)
+        self.ventana.SaveBtn.clicked.connect(self.SaveChrono)
+        self.ventana.SavedList.itemDoubleClicked.connect(self.LaunchChrono)
 
     # Implementados
+    def SaveChrono(self):
+        green_time = self.ventana.GreenEdit.text()
+        yellow_time = self.ventana.YellowEdit.text()
+        red_time = self.ventana.RedEdit.text()
+        chron_label = self.ventana.LabelEdit.text()
+        self.ventana.SavedList.addItem(chron_label+':'+green_time+','+yellow_time+','+red_time)
+
+
     def LaunchChrono(self):
         os.system("./chronometer.py 1")
