36548744d98251aea45f77b252af2b898a89c315
ceneha <elopez@fich.unl.edu.ar>
2014-09-16 01:01:39 -0300

    metadata, autor, licencia, email, etc

--- qt_chronometer.py
+++ qt_chronometer.py
@@ -1,3 +1,16 @@
+# -*- encoding: utf8 -*-
+
+#===============================================================================
+# META
+#===============================================================================
+
+__version__ = "0.2.0"
+__license__ = "GPL v3"
+__author__ = "Emiliano López"
+__email__ = "emiliano dot lopez at gmail dot com"
+__url__ = "https://github.com/emilopez/Conference-Chronometer"
+__date__ = "2014-09-16"
+
 from ui import qt_chrono_class
 import sys
 
